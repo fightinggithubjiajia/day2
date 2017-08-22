0f02a9f228be9f8e3af65288a663ce64e3541298
aa <aa>
2017-08-22 10:00:51 +0800

    第二次提交

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,8 +4,9 @@ package test;
  * Created by Administrator on 2017/8/22 0022.
  */
 public class Test {
-
+int i=9;
     public static void main(String[] args) {
+
         System.out.println("6666");
     }
 }
